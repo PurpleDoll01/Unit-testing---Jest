96d80b2cb3eb8ad53ec002fae9d79ef412b3caa1
Mariana Valencia <purple.mv@gmail.com>
2020-02-26 21:30:37 -0500

    Implementing tests for promises and async/await

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -19,6 +19,8 @@ test('Verdadero booleano', () => {
     expect(true).toBeTruthy();
 })
 
+//Probando callbacks
+
 const reverseString = (str, callback) => {
     callback(str.split("").reverse().join(""))
 }
@@ -28,3 +30,40 @@ test('Probar un Callback', () => {
         expect(str).toBe('aloH');
     });
 });
+
+//Probando promesas
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if(!str) {
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    });
+};
+
+test('Probar una promesa', () => {
+    return reverseString2('Hola')
+        .then(string => {
+            expect(string).toBe('aloH')
+        });
+});
+
+//Probando async y await
+
+test('Probar async/await', async () => {
+    const string = await reverseString2('Hola');
+    expect(string).toBe('aloH');
+});
+
+afterEach(() => console.log('Después de cada prueba'))
+//Permite correr código que se necesita después de cada prueba
+
+afterAll(() => console.log('Después de todas las pruebas'));
+//Permite correr código que se necesita después de todas las pruebas
+
+beforeEach(() => console.log('Antes de cada prueba'));
+// Permite correr --- antes de cada una de las las pruebas
+
+beforeAll(() => console.log('Antes de todas las pruebas'));
+//Permite correr -- antes de todas las pruebas
